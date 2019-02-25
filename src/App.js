8aa937980b3fae3946dcce211db0a69f74f339af
Lee Doughty <leedoughtydesign@gmail.com>
2019-02-25 10:11:14 +0000

    Update styling to header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Container>
         <Header />
-      </div>
+      </Container>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+  top: 20px;
+  left: 0;
+  right: 0;
+  position: fixed;
+`;
+
+const Title = styled.div`
+  position: absolute;
+  font-size: 2em;
+  left: 30px;
+`;
+
+const About = styled.div`
+  position: absolute;
+  font-size: 2em;
+  right: 30px;
+`;
 
 class Header extends Component {
   render() {
     return (
-      <h1>Giseigo</h1>
+      <HeaderContainer>
+        <Title>Giseigo</Title>
+        <About>About</About>
+      </HeaderContainer>
     )
   }
 }
